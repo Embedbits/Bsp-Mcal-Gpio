47af3831e7dea41cc9b1c3890179e20176f31d93
BSP Release <release@bsp>
2026-04-20 15:45:21 +0000

    Release v0.0.1 for STM32H5

---
+++ Gpio.h
@@ -0,0 +1,36 @@
+/**
+ * \author Mr.Nobody
+ * \file Gpio.h
+ * \ingroup Gpio
+ * \brief General-Purpose Input/Output (GPIO) peripheral handle module common
+ *        functionality header file.
+ *
+ * This file contains the common functionality used internally by the module,
+ * and shall provide interface between the module and the application.
+ *
+ */
+
+#ifndef GPIO_GPIO_H
+#define GPIO_GPIO_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ============================= INCLUDES =================================== */
+#include "Gpio_Types.h"                     /* Module types definition        */
+/* ============================= TYPEDEFS =================================== */
+
+/* ========================= SYMBOLIC CONSTANTS ============================= */
+
+/* ========================= EXPORTED MACROS ================================ */
+
+/* ========================= EXPORTED VARIABLES ============================= */
+
+/* ======================== EXPORTED FUNCTIONS ============================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GPIO_GPIO_H */
